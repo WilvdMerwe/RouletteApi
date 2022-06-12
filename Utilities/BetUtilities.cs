19f5770d8479ff61a6ffbb59141f5e6bdf7ba250
Wilhelm van der Merwe <wilvdmerwe@gmail.com>
2022-06-13 01:31:18 +0200

    Added BetUtilities

---
+++ Utilities/BetUtilities.cs
@@ -0,0 +1,16 @@
+﻿using RouletteApi.Enums;
+
+namespace RouletteApi.Utilities;
+
+public static class BetUtilities
+{
+    public static Color GetColorByNumber(int number)
+    {
+        if (number == 0)
+            return Color.Green;
+
+        var isEven = number % 2 == 0;
+
+        return isEven ? Color.Red : Color.Black;
+    }
+}
