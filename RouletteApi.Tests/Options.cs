6b3311b6ef2656bea0f3b1166796c41678761941
Wilhelm van der Merwe <wilvdmerwe@gmail.com>
2022-06-15 13:55:51 +0200

    Reshaped solution to include Test projec tinn git repo

---
+++ RouletteApi.Tests/Options.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouletteApi.Repositories;
+
+namespace RouletteApi.Tests;
+
+public static class Options
+{
+    public static DbContextOptions<RouletteDbContext> MockDbContextOptions => new DbContextOptionsBuilder<RouletteDbContext>()
+        .UseInMemoryDatabase(databaseName: "RouletteMockDb")
+        .Options;
+}
---
+++ RouletteApi.Tests/Services/UserServiceTest.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using RouletteApi.Models.Requests;
+using RouletteApi.Repositories;
+using RouletteApi.Services;
+
+namespace RouletteApi.Tests.Services;
+
+public class UserServiceTest
+{
+    // Arrange
+    private readonly List<UserCreateRequest> _validMockUserCreateRequests = new()
+    {
+        new UserCreateRequest
+        {
+            Email = "c137@counselofricks.com",
+            Name = "Rick",
+            Surname = "Sanchez"
+        },
+        new UserCreateRequest
+        {
+            Email = "morty.smith@gmail.com",
+            Name = "Morty",
+            Surname = "Smith"
+        }
+    };
+
+    private readonly List<UserCreateRequest> _invalidMockUserCreateRequests = new()
+    {
+        new UserCreateRequest
+        {
+            Email = "c137",
+            Name = "Rick",
+            Surname = "Sanchez"
+        },
+        new UserCreateRequest
+        {
+            Email = "morty.smith@gmail.com",
+            Name = "Morty",
+            Surname = ""
+        }
+    };
+
+    [Fact]
+    public async void CreateUsers_Failure_Validation()
+    {
+        // Act
+        await CreateMockUsers(_invalidMockUserCreateRequests, false);
+    }
+
+    [Fact]
+    public async void CreateUsers_Success()
+    {
+        // Act
+        await CreateMockUsers(_validMockUserCreateRequests, true);
+    }
+
+    private async Task CreateMockUsers(List<UserCreateRequest> userCreateRequests, bool isSuccessTest)
+    {
+        using (var context = new RouletteDbContext(Options.MockDbContextOptions))
+        {
+            var userRepo = new UserRepo(context);
+
+            var userService = new UserService(new NullLogger<UserService>(), userRepo);
+
+            foreach (var user in userCreateRequests)
+            {
+                var response = await userService.Create(user);
+
+                // Assert
+                var passed = response.Success == isSuccessTest;
+                Assert.True(passed);
+            }
+        }
+    }
+
+    [Fact]
+    public async void GetAllUsers_Success()
+    {
+        // Act
+        using (var context = new RouletteDbContext(Options.MockDbContextOptions))
+        {
+            var userRepo = new UserRepo(context);
+
+            var userService = new UserService(new NullLogger<UserService>(), userRepo);
+
+            var response = await userService.GetAll();
+
+            // Assert
+            Assert.True(response.Success);
+            Assert.Equal(_validMockUserCreateRequests.Count, response.Result.Count());
+        }
+    }
+
+    [Fact]
+    public async void UpdateUser_Failure_Validation()
+    {
+        // Arrange
+        var userUpdateRequest = new UserUpdateRequest
+        {
+            Name = "",
+            Surname = "Potter",
+        };
+
+        // Act
+        using (var context = new RouletteDbContext(Options.MockDbContextOptions))
+        {
+            var userRepo = new UserRepo(context);
+
+            var userService = new UserService(new NullLogger<UserService>(), userRepo);
+
+            var response = await userService.Update(1, userUpdateRequest);
+
+            // Assert
+            Assert.True(!response.Success);
+        }
+    }
+
+    [Fact]
+    public async void UpdateUser_Success()
+    {
+        // Arrange
+        var userUpdateRequest = new UserUpdateRequest
+        {
+            Name = "Harry",
+            Surname = "Potter",
+        };
+
+        // Act
+        using (var context = new RouletteDbContext(Options.MockDbContextOptions))
+        {
+            var userRepo = new UserRepo(context);
+
+            var userService = new UserService(new NullLogger<UserService>(), userRepo);
+
+            var response = await userService.Update(1, userUpdateRequest);
+
+            // Assert
+            Assert.True(response.Success);
+        }
+    }
+
+    [Fact]
+    public async void DeleteUser_Success()
+    {
+        // Act
+        using (var context = new RouletteDbContext(Options.MockDbContextOptions))
+        {
+            var userRepo = new UserRepo(context);
+
+            var userService = new UserService(new NullLogger<UserService>(), userRepo);
+
+            var response = await userService.DeleteById(2);
+
+            // Assert
+            Assert.True(response.Success);
+        }
+    }
+}
